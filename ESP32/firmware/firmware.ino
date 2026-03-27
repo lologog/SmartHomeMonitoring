485de4f37163f05d3eadde0c5dcef5551c4c353f
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2026-03-27 20:35:22 +0100

    feat: Successful connection with WiFi

--- ESP32/firmware/firmware.ino
+++ ESP32/firmware/firmware.ino
@@ -1,10 +1,32 @@
+#include "secrets.h"
+#include <WiFi.h>
+
+void connectWiFi()
+{
+    Serial.print("Connecting with WiFi");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.println("Connected to WiFi");
+    Serial.print("Obtained IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
 void setup() 
 {
-  Serial.begin(115200);
-  delay(1000);
+    Serial.begin(115200);
+    delay(1000);
+
+    connectWiFi();
 }
 
 void loop() 
 {
-  delay(1000);
+    delay(1000);
 }
